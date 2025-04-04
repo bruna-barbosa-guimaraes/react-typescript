7ae4f9366fb9ece24332089b351f87f373a84782
Bruna Guimarães <62905224+bruna-barbosa-guimaraes@users.noreply.github.com>
2025-04-04 08:39:05 -0300

    Primeiro commit

---
+++ src/pages/App.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Form from "../components/Form";
+import List from "../components/List";
+import style from "./App.module.scss";
+import Timer from "../components/Timer";
+
+function App() {
+  return (
+    <div className={style.AppStyle}>
+      <Form />
+      <List />
+      <Timer />
+    </div>
+  );
+}
+
+export default App;
